8ef9fd620a568d1258765ada67c5a4b212d90c15
RItesh Khandagale <ritesh,khandagale06@gmail.com>
2026-07-31 14:59:28 +0530

    Connect frontend to Render backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.104:5000",
+  baseURL: "https://queuetrack-order-tracking-system.onrender.com",
 });
 
 export default api;
